645bc27ad0b884c8c639091d30dfe2bf37062a79
bojannast <72553597+bojannast@users.noreply.github.com>
2020-10-16 17:30:02 +0200

    Added deletemapping

--- Bookstore/src/main/java/com/emts/books/Bookstore/api/CustomerController.java
+++ Bookstore/src/main/java/com/emts/books/Bookstore/api/CustomerController.java
@@ -4,7 +4,9 @@ import com.emts.books.Bookstore.model.Customer;
 import com.emts.books.Bookstore.security.SecurityService;
 import com.emts.books.Bookstore.service.CustomerService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -32,5 +34,12 @@ public class CustomerController {
     public Customer findAuthenticatedCustomer()
     {
         return securityService.getAuthenticatedCustomer();
+    }
+
+    @DeleteMapping("/api/customers/{id}")
+    public void deleteById(@PathVariable Long id) {
+
+        customerService.deleteById(id);
+
     }
 }
